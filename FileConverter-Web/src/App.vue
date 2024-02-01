54a48e38a47097cdbef8a92041529e348aa58420
Sakthikarthick <sakthikarthick3107@gmail.com>
2024-02-01 20:54:43 +0530

    vue integrate and tested

--- FileConverter-Web/src/App.vue
+++ FileConverter-Web/src/App.vue
@@ -1,30 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main>
+      <v-container>
+        <input type="file" @change="uploadFile" id="file" name="document"/>
+        <v-btn v-if="downloadUrl" @click="downloadFile" >Download</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    downloadUrl : null
+  }),
+  methods:{
+    uploadFile(event){
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('document' , file);
+
+      fetch('http://127.0.0.1:8000/conversion/doc-to-pdf/',{
+        method : 'POST',
+        body: formData
+      })
+      .then((response) => response.blob())
+      .then((blob) => {
+        this.downloadUrl = window.URL.createObjectURL(new Blob([blob]));
+       
+      })
+      .catch((err) => console.log('error' ,err))
+    },
+    downloadFile(){
+      const link = document.createElement('a');
+      link.href = this.downloadUrl;
+      link.setAttribute('download' , 'converted.pdf');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
