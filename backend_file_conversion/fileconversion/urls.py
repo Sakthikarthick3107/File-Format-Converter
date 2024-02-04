3c8273ed28b36073e26400ec9288353e96e19fbe
Sakthikarthick <sakthikarthick3107@gmail.com>
2024-02-04 23:13:59 +0530

    one url added jpg-to-png

--- backend_file_conversion/fileconversion/urls.py
+++ backend_file_conversion/fileconversion/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ConvertDocxToPdf , ConvertPdfToDocx , ConvertPngToJpg
+from .views import ConvertDocxToPdf , ConvertPdfToDocx , ConvertPngToJpg , ConvertJpgToPng
 
 urlpatterns = [
     path('docx-to-pdf/' , ConvertDocxToPdf.as_view() , name="docx-to-pdf"),
     path('pdf-to-docx/' , ConvertPdfToDocx.as_view() , name='pdf-to-docx'),
-    path('png-to-jpg/' , ConvertPngToJpg.as_view() , name='png-to-jpg')
+    path('png-to-jpg/' , ConvertPngToJpg.as_view() , name='png-to-jpg'),
+    path('jpg-to-png/' , ConvertJpgToPng.as_view() , name='jpg-to-png')
 ]
